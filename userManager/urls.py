a14fe800e249378bcd7da363ff865665dd162344
AndresGroselj <61983866+AndresGroselj@users.noreply.github.com>
2022-09-13 23:03:37 -0300

    login and create user gui

--- userManager/urls.py
+++ userManager/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = "userManager"
 
 urlpatterns = [
+    path('login', views.Login, name="Login"),
+    path('createUser', views.CreateUser, name="CreateUser"),
 ]
--- userManager/views.py
+++ userManager/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def Login(request):
+    context = {
+    }
+    return render(request, "userManager/login.html", context)
+
+def CreateUser(request):
+    context = {
+    }
+    return render(request, "userManager/createUser.html", context)
