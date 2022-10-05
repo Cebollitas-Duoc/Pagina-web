9aa312eb17348697c9c9f623edeef8d326dd7597
AndresGroselj <61983866+AndresGroselj@users.noreply.github.com>
2022-10-04 22:37:10 -0300

    fixed default user picture

--- departamentos/custom_context_processor.py
+++ departamentos/custom_context_processor.py
@@ -1,23 +1,22 @@
 from userManager.authAPI import *
 
 def user_renderer(request):
-    data = {}
+    data = {
+        "isLogged": False,
+        "usrName": "",
+        "usrImg": "/img/profiles/default.png",
+    }
     if ("SessionKey" in request.COOKIES):
         profileData = getSessionProfile(request)
 
         if (profileData["ValidSession"]):
             name = profileData["Name"]
             lastName = profileData["LastName"]
-            data = {
-            "isLogged": True,
-            "usrName": f"{name} {lastName}",
-            "usrImg": profileData["Picture"],
-            }
-        else:
-            data = {
-            "isLogged": False,
-            "usrName": "",
-            "usrImg": "/img/profiles/default.png",
-            }
+            data["isLogged"] = True
+            data["usrName"] = f"{name} {lastName}"
+            
+            if (profileData["Picture"] ):
+                data["usrImg"] = profileData["Picture"]
+            
 
     return data
