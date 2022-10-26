b24766d304bc0d823aa8a19a364233f495ccfd8c
Chris99fm <Chris99FM@outlook.com>
2022-10-26 17:08:22 -0300

    actualizacion depto.html

--- static/js/imagegallery.js
+++ static/js/imagegallery.js
@@ -4,5 +4,7 @@ const mainImage = document.querySelector(".main-image");
 function setMainImage(imageSRC){
   mainImage.src = imageSRC;
 }
-
-
+document.addEventListener('DOMContentLoaded', function() {
+  const firstMiniImage = document.querySelectorAll(".mini-image")[0];
+ mainImage.src = firstMiniImage.getAttribute("src");
+}, false);
