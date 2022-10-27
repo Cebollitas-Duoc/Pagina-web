c6f52be19aa2e175d320f3ce69bc7678f8227ea4
Chris99fm <Chris99FM@outlook.com>
2022-10-27 04:39:03 -0300

    misreservas

---
+++ static/js/cancelarreserva.js
@@ -0,0 +1,20 @@
+async function cancelar(Id_Reserva){
+    var formdata = new FormData();
+    var r
+    console.log(Id_Reserva);
+    formdata.append("SessionKey", getCookie("SessionKey"));
+    formdata.append("Id_Reserva", parseInt(Id_Reserva));
+    
+    var requestOptions = {
+        method: 'POST',
+        body: formdata,
+        redirect: 'follow'
+    };
+    
+    await fetch(`${apidomain}/reservas/cancelreserve/`, requestOptions)
+    .then(response => response.text())
+    .then(result => r=result)
+    .catch(error => console.log('error', error));
+
+    return r
+}
