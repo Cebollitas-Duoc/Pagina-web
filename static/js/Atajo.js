46e703b36457da60b556728e932accb81873137e
Elli4ss <70156348+Elli4ss@users.noreply.github.com>
2022-11-27 01:22:04 -0300

    Cambios css/JS
    Cambios css
    -Home separacion
    -Tarjetas departamentos
    -botones
    -mensaje de verificacion y errores
    Cambio en JS  Atajo

--- static/js/Atajo.js
+++ static/js/Atajo.js
@@ -1,33 +1,7 @@
-const botonlogin = document.getElementById("LoginButton")
-const botonCreateUser = document.getElementById("CreateUserButton")
-const botonUpdate = document.getElementById("UpdateProfileButton")
-const botonUpdatePassword = document.getElementById("ChangePasswordButton")
-
-
 window.addEventListener('keypress', (e) => {
-  if(e.key === 'Enter'){
-    console.log("*le clickea UWU")
-    botonlogin.click()
-  }
-});
+  const boton = document.querySelector(".submit")
 
-window.addEventListener('keypress', (e) => {
   if(e.key === 'Enter'){
-    console.log("*le clickea UWU")
-    botonCreateUser.click()
-  }
-});
-
-window.addEventListener('keypress', (e) => {
-  if(e.key === 'Enter'){
-    console.log("*le clickea UWU")
-    botonUpdate.click()
-  }
-});
-
-window.addEventListener('keypress', (e) => {
-  if(e.key === 'Enter'){
-    console.log("*le clickea UWU")
-    botonUpdatePassword.click()
+    boton.click()
   }
 });
