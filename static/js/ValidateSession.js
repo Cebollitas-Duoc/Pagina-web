ccd9fb81440ddd2aadc7c463d728248fa7418921
AndresGroselj <61983866+AndresGroselj@users.noreply.github.com>
2022-12-07 11:10:31 -0300

    feature: verificar si session es valida

---
+++ static/js/ValidateSession.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', async () =>{
+    const validationResult = await isSessionValid();
+    
+    if ("Valid" in validationResult)
+        setCookie("LogedIn", validationResult.Valid, 7)
+    else 
+        setCookie("LogedIn", false, 7)
+})
+
+
+async function isSessionValid(){  
+    var formdata = new FormData();
+    var r
+    formdata.append("SessionKey", getCookie("SessionKey"));
+    
+    var requestOptions = {
+        method: 'POST',
+        body: formdata,
+        redirect: 'follow'
+    };
+    
+    await fetch(`${apidomain}/auth/ValidateSession/`, requestOptions)
+    .then(response => response.text())
+    .then(result => r=result)
+    .catch(error => console.log('error', error));
+
+    return JSON.parse(r)
+}
