07d525e9a1c17089d8b4714bfa80590c8afafe84
Elli4ss <70156348+Elli4ss@users.noreply.github.com>
2022-12-07 01:14:09 -0300

    Nueva vista DetalleReservas
    Se creo nueva vista con los detalles de la reserva
    Cambios de css

--- reservas/urls.py
+++ reservas/urls.py
@@ -5,4 +5,6 @@ app_name = "reservas"
 
 urlpatterns = [
     path('mireserva', views.Reserva, name="reserva"),
+    path('ReservaDetalle/<int:_iddepto>', views.ReservaDetalle, name="ReservaDetalle")
 ]
+
--- reservas/views.py
+++ reservas/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .reservasAPI import *
+from userManager.authAPI import *
 # Create your views here.
 
 def Reserva(request):
@@ -8,3 +9,10 @@ def Reserva(request):
         "reservas" : reservas
     }
     return render(request, "reservas/mireserva.html", context)
+
+def ReservaDetalle(request, _iddepto):
+    depto = getDepto(_iddepto)
+    context = {
+        "depto" : depto
+    }
+    return render(request, "reservas/ReservaDetalle.html", context)
