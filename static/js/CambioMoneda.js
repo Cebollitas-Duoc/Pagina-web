3d2a7b971d7a4c6b7cc244dae05bec8c286a9c9f
Elli4ss <70156348+Elli4ss@users.noreply.github.com>
2022-12-08 02:49:10 -0300

    Formato moneda/Vista DetalleReserva
    Modificacion detalleReserva Html
    Modificacion detalleReserva Scss
    Cambios scss generales

--- static/js/CambioMoneda.js
+++ static/js/CambioMoneda.js
@@ -1,12 +1,12 @@
 const valor = document.getElementById('valor');
-const xd = document.getElementById('xd');
 const formatterPeso = new Intl.NumberFormat('es-CO', {
     style: 'currency',
     currency: 'COP',
     minimumFractionDigits: 0})
 
 window.addEventListener('load' ,(e)=>{
+    
     valueRaw = valor.getAttribute('value');
-    a = formatterPeso.format(parseInt(valueRaw))
-    xd.innerHTML = a+" CLP por noche"
+    ValorFormat = formatterPeso.format(parseInt(valueRaw))
+    valor.innerHTML = "Costo Final " + ValorFormat + " CLP "
 });
