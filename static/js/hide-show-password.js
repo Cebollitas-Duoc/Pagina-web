6a91bdbca6f9adcd51959cf5a03d7be8625c680b
AndresGroselj <61983866+AndresGroselj@users.noreply.github.com>
2022-12-11 23:41:47 -0300

    limpieza

--- static/js/hide-show-password.js
+++ static/js/hide-show-password.js
@@ -2,17 +2,15 @@ function revelar(checkbox, id, openid, closeid)
       {
          const open = document.getElementById(openid).src
          const close = document.getElementById(closeid).src
-         console.log(checkbox.getAttribute('check'), "wena loco")
       if(checkbox.getAttribute('check') === '0')
          {
-         console.log("0", 'uwu')
          checkbox.src = close
          document.getElementById(closeid).src = open
          checkbox.setAttribute('check', '1');
          document.getElementById(id).setAttribute('type', 'password');
       }
       else
-         {console.log("1", 'uwu')
+         {
          checkbox.src = close
          document.getElementById(closeid).src = open
          checkbox.setAttribute('check', '0');
