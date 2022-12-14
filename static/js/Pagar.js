ff2ecd1803d24c1ecdadcfc03917e29bffcd9fca
Chris99fm <Chris99FM@outlook.com>
2022-12-13 22:07:28 -0300

    validaciones pagar/cancelar

--- static/js/Pagar.js
+++ static/js/Pagar.js
@@ -1,7 +1,43 @@
 async function pagar(Id_Reserva){
+    console.log(Id_Reserva)
+    var estado = await verificar(Id_Reserva);
+    if(parseInt(estado) === 0){
+        var formdata = new FormData();
+        var r
+        formdata.append("Id_Reserva", parseInt(Id_Reserva));
+        
+        var requestOptions = {
+            method: 'POST',
+            body: formdata,
+            redirect: 'follow'
+        };
+        
+        await fetch(`${apidomain}/reservas/transbankpay/`, requestOptions)
+        .then(response => response.text())
+        .then(result => r=result)
+        .catch(error => console.log('error', error));
+        res = JSON.parse(r)
+        window.location.replace(res.link);
+    
+        return r
+    }
+    else if(parseInt(estado) === 1){
+        location.reload();
+        GlobalMessage.setGlobalSuccessMessage('La Reserva ya ha sido Pagada')
+    }
+    else if(parseInt(estado) === 2){
+        location.reload();
+        GlobalMessage.setGlobalSuccessMessage('La Reserva ya ha Finalizado')
+    }
+    else if(parseInt(estado) === 3){
+        location.reload();
+        GlobalMessage.setGlobalSuccessMessage('La Reserva ha sido Cancelada, por lo que no puede ser Pagada')
+    }
+}
+
+async function verificar(Id_Reserva){
     var formdata = new FormData();
     var r
-    formdata.append("Id_Reserva", parseInt(Id_Reserva));
     
     var requestOptions = {
         method: 'POST',
@@ -9,13 +45,10 @@ async function pagar(Id_Reserva){
         redirect: 'follow'
     };
     
-    await fetch(`${apidomain}/reservas/transbankpay/`, requestOptions)
+    await fetch(`${apidomain}/reservas/getuserreservebyid/`+Id_Reserva+'/', requestOptions)
     .then(response => response.text())
     .then(result => r=result)
     .catch(error => console.log('error', error));
     res = JSON.parse(r)
-    console.log(res.link)
-    window.location.replace(res.link);
-
-    return r
+    return res.ID_ESTADORESERVA
 }
--- static/js/cancelarreserva.js
+++ static/js/cancelarreserva.js
@@ -7,14 +7,47 @@ async function cancelar(Id_Reserva){
     else if ("Error" in response) 
         GlobalMessage.printGlobalErrorMessage(response["Error"])
     else
-        GlobalMessage.printGlobalErrorMessage("Error desconocido al crear reserva")
+        GlobalMessage.printGlobalErrorMessage("Error desconocido al cancelar la reserva")
 }
 
 async function cancelRequest(Id_Reserva){
+    var estado = verificar(Id_Reserva)
+    if(parseInt(estado) === 0){
+        var formdata = new FormData();
+        var r
+        formdata.append("SessionKey", getCookie("SessionKey"));
+        formdata.append("Id_Reserva", parseInt(Id_Reserva));
+        
+        var requestOptions = {
+            method: 'POST',
+            body: formdata,
+            redirect: 'follow'
+        };
+        
+        await fetch(`${apidomain}/reservas/cancelreserve/`, requestOptions)
+        .then(response => response.text())
+        .then(result => r=result)
+        .catch(error => console.log('error', error));
+        
+        return JSON.parse(r);
+    }
+    else if(parseInt(estado) === 1){
+        location.reload();
+        GlobalMessage.setGlobalSuccessMessage('La Reserva ya ha sido Pagada')
+    }
+    else if(parseInt(estado) === 2){
+        location.reload();
+        GlobalMessage.setGlobalSuccessMessage('La Reserva ya ha Finalizado')
+    }
+    else if(parseInt(estado) === 3){
+        location.reload();
+        GlobalMessage.setGlobalSuccessMessage('La Reserva ya ha sido Cancelada')
+    }
+}
+
+async function verificar(Id_Reserva){
     var formdata = new FormData();
     var r
-    formdata.append("SessionKey", getCookie("SessionKey"));
-    formdata.append("Id_Reserva", parseInt(Id_Reserva));
     
     var requestOptions = {
         method: 'POST',
@@ -22,10 +55,10 @@ async function cancelRequest(Id_Reserva){
         redirect: 'follow'
     };
     
-    await fetch(`${apidomain}/reservas/cancelreserve/`, requestOptions)
+    await fetch(`${apidomain}/reservas/getuserreservebyid/`+Id_Reserva+'/', requestOptions)
     .then(response => response.text())
     .then(result => r=result)
     .catch(error => console.log('error', error));
-    
-    return JSON.parse(r);
+    res = JSON.parse(r)
+    return res.ID_ESTADORESERVA
 }
